c03dd3055f8889c696914afd60486767bd8acc55
Swapnil Dixit <swapnil333@msn.com>
2018-05-08 23:22:12 +0100

    test checkin

--- UserAPI/Startup.cs
+++ UserAPI/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UserAPI.Model;
-using UserAPI.MongoRepository;
+using UserAPI.MongoRepository; 
 
 namespace UserAPI
 {
